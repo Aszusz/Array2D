b060cd360b51c3cdd715567e3e2309472f91876f
Adrian Szuszkiewicz <adrianszuszkiewicz@gmail.com>
2020-06-21 15:05:30 +0200

    Create Array2D module with tests

---
+++ src/Array2D.elm
@@ -0,0 +1,57 @@
+module Array2D exposing (Array2D, Position, Size, cross, empty, get, indexedMap)
+
+import Array exposing (Array, indexedMap)
+
+
+type alias Array2D a =
+    Array (Array a)
+
+
+type alias Size =
+    { cols : Int
+    , rows : Int
+    }
+
+
+type alias Position =
+    { rowIndex : Int
+    , colIndex : Int
+    }
+
+
+empty : Array2D a
+empty =
+    Array.fromList [ Array.fromList [] ]
+
+
+get : Position -> Array2D a -> Maybe a
+get pos arr =
+    arr
+        |> Array.get pos.rowIndex
+        |> Maybe.andThen (Array.get pos.colIndex)
+
+
+map : (a -> b) -> Array2D a -> Array2D b
+map func arr =
+    let
+        mapRow row =
+            row |> Array.map func
+    in
+    arr |> Array.map mapRow
+
+
+indexedMap : (Position -> a -> b) -> Array2D a -> Array2D b
+indexedMap func arr =
+    let
+        mapElement rowIndex colIndex element =
+            element |> func { rowIndex = rowIndex, colIndex = colIndex }
+
+        mapRow rowIndex row =
+            row |> Array.indexedMap (mapElement rowIndex)
+    in
+    arr |> Array.indexedMap mapRow
+
+
+cross : (a -> b -> result) -> Array a -> Array b -> Array2D result
+cross func xs ys =
+    Array.map (\x -> Array.map (\y -> func x y) ys) xs
---
+++ tests/Example.elm
@@ -0,0 +1,53 @@
+module Example exposing (suite)
+
+import Array
+import Array2D
+import Expect
+import Test exposing (Test, describe, test)
+import Tuple as Tuple
+
+
+suite : Test
+suite =
+    let
+        array1 =
+            Array.fromList [ "A", "B", "C" ]
+
+        array2 =
+            Array.fromList [ 1, 2, 3 ]
+
+        array3 =
+            Array.fromList
+                [ Array.fromList [ ( "A", 1 ), ( "A", 2 ), ( "A", 3 ) ]
+                , Array.fromList [ ( "B", 1 ), ( "B", 2 ), ( "B", 3 ) ]
+                , Array.fromList [ ( "C", 1 ), ( "C", 2 ), ( "C", 3 ) ]
+                ]
+
+        array4 =
+            Array.fromList
+                [ Array.fromList [ ( 1, 1 ), ( 1, 2 ), ( 1, 3 ) ]
+                , Array.fromList [ ( 2, 1 ), ( 2, 2 ), ( 2, 3 ) ]
+                , Array.fromList [ ( 3, 1 ), ( 3, 2 ), ( 3, 3 ) ]
+                ]
+    in
+    describe "Math tests"
+        [ test "List2D is a cross product of two lists" <|
+            \_ ->
+                let
+                    actual =
+                        Array2D.cross Tuple.pair array1 array2
+                in
+                Expect.equal actual array3
+        , test "get" <|
+            let
+                actual =
+                    Array2D.get { rowIndex = 2, colIndex = 1 } array3
+            in
+            \_ -> Expect.equal actual <| Just ( "C", 2 )
+        , test "indexedMap" <|
+            let
+                actual =
+                    Array2D.indexedMap (\pos _ -> ( pos.rowIndex + 1, pos.colIndex + 1 )) array3
+            in
+            \_ -> Expect.equal actual array4
+        ]
